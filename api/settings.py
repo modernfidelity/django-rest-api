91a59ae7b9808c14aed4c5b4c4fd6b0a273c79d6
Mark Rushton <mark@modernfidelity.co.uk>
2016-02-04 21:19:37 +0000

    custom storage s3 classes for STATIC + MEDIA files

--- api/settings.py
+++ api/settings.py
@@ -170,7 +170,7 @@ MEDIA_URL = '/media/'
 AWS_ACCESS_KEY_ID = os.environ.get('DRF_AWS_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.environ.get('DRF_AWS_SECRET_KEY')
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('DRF_AWS_S3_BUCKET')
 
@@ -191,8 +191,18 @@ AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+# STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+#
+# # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# # you run `collectstatic`).
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
-# you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
